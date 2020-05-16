666c7faeb3ee488817d4c54cd3f0f84a1fe89619
Temitope <tope_olajide@outlook.com>
2020-05-16 04:31:33 +0100

    Configure MongoDB

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,16 @@
 import express, { Application } from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
-import * as mongoose from "mongoose";
+import mongoose from "mongoose";
 
 class App {
   public app: Application;
-  public mongoUrl: string = 'mongodb://localhost/CRMdb';
+  public mongoUrl: string = 'mongodb://localhost/Blogcast';
   constructor() {
     this.app = express();
     this.setConfig();
+    this.mongoSetup();
+
   }
 
   private setConfig() {
@@ -16,11 +18,20 @@ class App {
     this.app.use(bodyParser.json({ limit: '50mb' }));
 
     //Allows us to receive requests with data in x-www-form-urlencoded format
-    this.app.use(bodyParser.urlencoded({ limit: '50mb', extended:true}));
+    this.app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
     //Enables cors   
     this.app.use(cors());
   }
+  private mongoSetup(): void {
+    mongoose.connect(this.mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
+    var db = mongoose.connection;
+    db.on('error', console.error.bind(console, 'connection error:'));
+    db.once('open', function () {
+      console.log("we're connected!")
+    });
+  }
+
 }
 
 export default new App().app;
